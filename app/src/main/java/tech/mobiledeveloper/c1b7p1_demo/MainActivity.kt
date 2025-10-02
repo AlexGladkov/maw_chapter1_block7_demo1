297d6b8394cf7ac5f8f28eb8559c601e5a9eb788
Mobile Developer <bob298@yandex.ru>
2025-10-02 15:19:53 +0300

    initial commit

---
+++ app/src/main/java/tech/mobiledeveloper/c1b7p1_demo/MainActivity.kt
@@ -0,0 +1,47 @@
+package tech.mobiledeveloper.c1b7p1_demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import tech.mobiledeveloper.c1b7p1_demo.ui.theme.C1B7P1_DemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            C1B7P1_DemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    C1B7P1_DemoTheme {
+        Greeting("Android")
+    }
+}
